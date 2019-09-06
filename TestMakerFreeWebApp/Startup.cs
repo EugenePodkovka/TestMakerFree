7346ea4e31a1cfdb2882efca3952f51c8e7d2a9d
Grizo <eugene.podkovka@gmail.com>
2019-09-06 22:21:56 +0300

    adding and linking connectionstring

--- TestMakerFreeWebApp/Startup.cs
+++ TestMakerFreeWebApp/Startup.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TestMakerFreeWebApp.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace TestMakerFreeWebApp
 {
@@ -23,6 +21,9 @@ namespace TestMakerFreeWebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddEntityFrameworkSqlServer();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
