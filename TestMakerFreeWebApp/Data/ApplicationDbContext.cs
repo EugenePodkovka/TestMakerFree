b24cb1112718def3e06ab032c885516b9f109462
Grizo <eugene.podkovka@gmail.com>
2019-09-06 21:58:24 +0300

    setting up the dbcontext

---
+++ TestMakerFreeWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace TestMakerFreeWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) :
+            base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ApplicationUser>().ToTable("Users");
+            modelBuilder.Entity<ApplicationUser>().HasMany(u => u.Quizzes).WithOne(i => i.User);
+
+            modelBuilder.Entity<Quiz>().ToTable("Quizzes");
+            modelBuilder.Entity<Quiz>().Property(i => i.Id).ValueGeneratedOnAdd();
+            modelBuilder.Entity<Quiz>().HasOne(i => i.User).WithMany(u => u.Quizzes);
+            modelBuilder.Entity<Quiz>().HasMany(i => i.Questions).WithOne(c => c.Quiz);
+            modelBuilder.Entity<Quiz>().HasMany(i => i.Results).WithOne(r => r.Quiz);
+
+            modelBuilder.Entity<Question>().ToTable("Questions");
+            modelBuilder.Entity<Question>().Property(q => q.Id).ValueGeneratedOnAdd();
+            modelBuilder.Entity<Question>().HasOne(q => q.Quiz).WithMany(q => q.Questions);
+            modelBuilder.Entity<Question>().HasMany(q => q.Answers).WithOne(a => a.Question);
+
+            modelBuilder.Entity<Answer>().ToTable("Answers");
+            modelBuilder.Entity<Answer>().Property(a => a.Id).ValueGeneratedOnAdd();
+            modelBuilder.Entity<Answer>().HasOne(a => a.Question).WithMany(q => q.Answers);
+
+            modelBuilder.Entity<Result>().ToTable("Results");
+            modelBuilder.Entity<Result>().Property(r => r.Id).ValueGeneratedOnAdd();
+            modelBuilder.Entity<Result>().HasOne(r => r.Quiz).WithMany(q => q.Results);
+
+        }
+
+        public DbSet<ApplicationUser> Users { get; set; }
+        public DbSet<Quiz> Quizzes { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+        public DbSet<Result> Results { get; set; }
+    }
+}
